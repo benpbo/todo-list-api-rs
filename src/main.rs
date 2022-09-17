781bc45a5881ada428a0739ebbd8835b20ee4604
benpbo <prizeben@gmail.com>
2022-09-17 17:30:30 +0300

    feat: add logging

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod infrastructure;
 
 use actix::Actor;
 use actix_web::{
+    middleware::Logger,
     web::{get, post, resource, Data, ServiceConfig},
     App, HttpServer,
 };
@@ -15,11 +16,14 @@ use std::io;
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
+    env_logger::init();
+
     let task_repository = InMemoryTaskRepository::new();
     let task_service = TaskService::new(task_repository).start();
 
     let app = move || {
         App::new()
+            .wrap(Logger::default())
             .app_data(Data::new(task_service.clone()))
             .configure(routes)
     };
